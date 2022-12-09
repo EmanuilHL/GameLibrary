c3f0aa53977a793f6768ea7af9c05a93294738ae
emanu <emanu@EMO>
2022-12-09 21:28:49 +0100

    CareerTests almost done.

--- GameLibrary.Tests/UnitTests/CareerServiceClass.cs
+++ GameLibrary.Tests/UnitTests/CareerServiceClass.cs
@@ -1,6 +1,11 @@
 ﻿using GameLibrary.Core.Contracts;
 using GameLibrary.Core.Services;
+using GameLibrary.Infrastructure.Data;
 using GameLibrary.Infrastructure.Data.Common;
+using GameLibrary.Infrastructure.Data.Entities;
+using GameLibrary.Tests.Mocks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 using Moq;
 using System;
 using System.Collections.Generic;
@@ -11,42 +16,111 @@ using System.Threading.Tasks;
 namespace GameLibrary.Tests.UnitTests
 {
     [TestFixture]
-    public class CareerServiceClass : UnitTestsBase
+    public class CareerServiceClass
     {
+        private IRepository repo;
         private ICareerService careerService;
-        //private readonly IRepository repo;
-        protected Mock<IRepository> repoMock;
+        private ApplicationDbContext context;
 
-        //[SetUp]
-        //public void SetUp()
-        //{
-        //    repoMock = new Mock<IRepository>(data);
-        //    this.careerService = new CareerService(repoMock);
-        //}
+        [SetUp]
+        public void Setup()
+        {
+            context = DatabaseMock.Data;
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            repo = new Repository(context);
+
+            var cacheMock = new Mock<IMemoryCache>();
+            var cache = cacheMock.Object;
+            careerService = new CareerService(repo, cache);
+        }
 
         [Test]
-        public async Task ExistsById_ShouldReturnTrue()
+        public async Task ExistsById_AndHelperWithPhoneNumber_ShouldReturnTrueAndFalse()
         {
-            //var mockCreditDecisionService = new Mock<ICreditDecisionService>();
-            //mockCreditDecisionService
-            //    .Setup(p => p.GetDecision(100))
-            //    .Returns("Declined");
-
-            //var controller = new CreditDecision(mockCreditDecisionService.Object);
-            //var result = controller.MakeCreditDecision(100);
-
-            //Assert.That(result, Is.EqualTo("Declined"));
-
             //Arrange
-            //var mockCareerService = new Mock<ICareerService>();
-            //mockCareerService
-            //    .Setup(p => p.ExistsById(this.User2.Id))
-            //    .Returns(Task.FromResult(true));
-            ////Act
-            //var choice = await careerService.ExistsById(this.User2.Id);
+            string userId1 = "1ca18236-2d0f-49ee-ab91-246c38c3c1d9";
+            string userId2 = "1786b223-314a-40ef-92e4-f7204b772971";
+            string phoneNumber = "+35914500233";
+            string phoneNumber2 = "+35914500243";
 
-            ////Assert
-            //Assert.That(choice, Is.EqualTo(true));
+            await repo.AddAsync(new Helper
+            {
+                PhoneNumber = phoneNumber,
+                UserId = userId1
+            });
+
+            IEnumerable<User> users = new List<User>()
+            {
+                new User
+                {
+                    Id = userId1
+                },
+                new User
+                {
+                    Id = userId2
+                }
+            };
+            await repo.AddRangeAsync(users);
+
+            await repo.SaveChangesAsync();
+
+            //Act
+            var truetest = await careerService.ExistsById(userId1);
+            var falsetest = await careerService.ExistsById(userId2);
+
+            var truePhonetest = await careerService.HelperWithPhoneNumberExists(phoneNumber);
+            var falsePhonetest = await careerService.HelperWithPhoneNumberExists(phoneNumber2);
+
+            //Assert
+            Assert.That(truetest, Is.True);
+            Assert.That(truePhonetest, Is.True);
+
+            Assert.That(falsetest, Is.False);
+            Assert.That(falsePhonetest, Is.False);
+        }
+
+        [Test]
+        public async Task CreateHelper_ReturnsHelperCreated()
+        {
+            //Arrange
+            string userId = "1786b223-314a-40ef-92e4-f7204b772971";
+            string phoneNumber = "+345032353214";
+            //Act
+            await careerService.CreateHelper(userId, phoneNumber);
+
+            var helper = await repo.All<Helper>().FirstOrDefaultAsync(x => x.UserId == userId && x.PhoneNumber == phoneNumber);
+            //Assert
+            Assert.That(helper.UserId, Is.EqualTo(userId));
+            Assert.That(helper.PhoneNumber, Is.EqualTo(phoneNumber));
+        }
+
+        //[Test]
+        //public async Task AllHelpers_ReturnsHelpersEvenNull()
+        //{
+        //    //Arrange
+        //    string userId = "1786b223-314a-40ef-92e4-f7204b772971";
+        //    string phoneNumber = "+345032353214";
+
+        //    await repo.AddAsync(new Helper()
+        //    {
+        //        UserId = userId,
+        //        PhoneNumber = phoneNumber
+        //    });
+
+        //    await repo.SaveChangesAsync();
+        //    //Act
+        //    var helpers = await careerService.AllHelpers();
+
+        //    //Assert
+        //    Assert.That(helpers.Count(), Is.EqualTo(1));
+        //}
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.context.Dispose();
         }
     }
 }
---
+++ GameLibrary.Tests/UnitTests/GameServiceTests.cs
@@ -0,0 +1,105 @@
+﻿using GameLibrary.Core.Contracts;
+using GameLibrary.Core.Services;
+using GameLibrary.Infrastructure.Data;
+using GameLibrary.Infrastructure.Data.Common;
+using GameLibrary.Infrastructure.Data.Entities;
+using GameLibrary.Tests.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary.Tests.UnitTests
+{
+    public class GameServiceTests
+    {
+        private IRepository repo;
+        private IGameService gameService;
+        private ApplicationDbContext context;
+
+        [SetUp]
+        public void Setup()
+        {
+            context = DatabaseMock.Data;
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+
+            repo = new Repository(context);
+            gameService = new GameService(repo);
+        }
+
+        [Test]
+        public async Task IsUserDevelepor_ReturnsTrueAndFalseWithTwoUsers()
+        {
+            //Arrange
+            string userId1 = "1ca18236-2d0f-49ee-ab91-246c38c3c1d9";
+            string userId2 = "1786b223-314a-40ef-92e4-f7204b772971";
+
+            await repo.AddAsync(new Game
+            {
+                Title = "",
+                ImageUrl = "",
+                Description = "",
+                UserId = userId1
+            });
+
+            IEnumerable<User> users = new List<User>()
+            {
+                new User
+                {
+                    Id = userId1
+                },
+                new User
+                {
+                    Id = userId2
+                }
+            };
+            await repo.AddRangeAsync(users);
+
+            await repo.SaveChangesAsync();
+
+            //Act
+            var truetest = await gameService.IsUserDevelepor(userId1);
+            var falsetest = await gameService.IsUserDevelepor(userId2);
+
+            //Assert
+            Assert.That(truetest, Is.True);
+            Assert.That(falsetest, Is.False);
+        }
+
+        [Test]
+        public async Task GetGamesCreatedByUserId_ReturnsTheGamesByUserId()
+        {
+            //Arrange
+            string userId = "6c0e1cc2-e4d5-4b77-a9ae-c88a0d2bd184";
+            await repo.AddAsync(new Game()
+            {
+                Id = 1,
+                Description = "",
+                ImageUrl = "",
+                Rating = 10.00m,
+                Title = "League",
+                UserId = userId,
+                DislikesCount = 0,
+                LikesCount = 0
+            });
+
+            await repo.SaveChangesAsync();
+            //Act
+            var games = await gameService.GetGamesCreatedByUserId(userId);
+            var game = games.FirstOrDefault(x => x.UserId == userId);
+
+            Assert.That(games.Count(), Is.EqualTo(1));
+            Assert.That(game.Title, Is.EqualTo("League"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.context.Dispose();
+        }
+    }
+}
